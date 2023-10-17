60e5bc93edf45124bcc80f70ce475074888c3d52
jenzvandevelde <jens.van.de.velde@icloud.com>
2023-10-17 10:22:19 +0200

    c# added

---
+++ Project_CG_Csharp/CG.StartUp/Program.cs
@@ -0,0 +1,22 @@
+﻿using CG.Application.Repositorys;
+using CG.Persistence;
+using CollectAndGO.Application;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CG.StartUp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IRecipeRepository recipeRepo = new RecipeMapper();
+            DomainManager manager = new DomainManager(recipeRepo);
+
+            manager.GetRecipes().ForEach(r => {
+                Console.WriteLine(r.ToString());
+                Console.WriteLine();    
+            });  ;
+            
+        }
+    }
+}
---
+++ Project_CG_Csharp/Project_Collect_and_Go_t5/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Project_Collect_and_Go_t5
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
