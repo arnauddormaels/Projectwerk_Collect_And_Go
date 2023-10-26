fc0955cb2abcb3d431bc3c29846613470f8aee31
arnauddormaels <arnaud.dormaels@student.hogent.be>
2023-10-26 17:13:40 +0200

    React toegevoegd, met nodige aanpassingen

---
+++ CG.React/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>HALLOOO IK BEN HIER OOK NU </h1>
+    </>
+  )
+}
+
+export default App
