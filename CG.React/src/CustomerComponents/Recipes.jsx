1c9fa270901ac063c85ae6e14b80c45a19f39b3c
jenzvandevelde <jens.van.de.velde@icloud.com>
2024-01-15 21:37:49 +0100

    added

--- CG.React/src/CustomerComponents/Recipes.jsx
+++ CG.React/src/CustomerComponents/Recipes.jsx
@@ -1,24 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import VideoPlayer from '../Videoscreen/Videoplayer'; // Importeer de VideoPlayer-component
+import VideoPlayer from '../Videoscreen/Videoplayer';
 import styles from '../css/Customer.module.css';
 
 const Recipes = () => {
   const [recipes, setRecipes] = useState([]);
-  const [selectedRecipe, setSelectedRecipe] = useState(null); // Houd het geselecteerde gerecht bij
-
-  const maxRecipesPerRow = 4;
-  const maxRecipesToShow = 4;
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
 
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
         const response = await fetch("https://localhost:7226/api/Recipe/getActiveRecipes");
         const data = await response.json();
-
-        const limitedRecipes = data.slice(0, maxRecipesToShow);
-        setRecipes(limitedRecipes);
+        setRecipes(data);
       } catch (error) {
-        throw new Error("Er ging iets mis met het ophalen van api/Recipe/getActiveRecipes");
+        console.error("Er ging iets mis met het ophalen van de recepten", error);
       }
     };
 
@@ -26,7 +21,7 @@ const Recipes = () => {
   }, []);
 
   const handleRecipeClick = (recipe) => {
-    setSelectedRecipe(recipe); // Wanneer er op een gerecht wordt geklikt, stel het geselecteerde gerecht in
+    setSelectedRecipe(recipe);
   };
 
   return (
@@ -37,20 +32,17 @@ const Recipes = () => {
             <div
               key={recipe.recipeId}
               className={styles.imageContainer}
-              onClick={() => handleRecipeClick(recipe)} // Voeg een click handler toe om het geselecteerde gerecht in te stellen
+              onClick={() => handleRecipeClick(recipe)}
             >
               <img src={recipe.imgUrl} alt={recipe.name} />
               <p className={styles.imageText}>{recipe.name}</p>
-              {index % maxRecipesPerRow === maxRecipesPerRow - 1 && (
-                <div className={styles.clear}></div>
-              )}
             </div>
           ))}
         </div>
       </div>
 
       {selectedRecipe && (
-        <VideoPlayer selectedRecipe={selectedRecipe} /> // Als er een geselecteerd gerecht is, geef het door aan de VideoPlayer-component
+        <VideoPlayer selectedRecipe={selectedRecipe} />
       )}
 
       <div className={styles.footer} style={{ margin: '100px' }} />
